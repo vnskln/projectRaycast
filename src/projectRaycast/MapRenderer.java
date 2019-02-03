92d7ffa66f39d0233dd02389752aadd487057573
vnskln <37518904+vnskln@users.noreply.github.com>
2019-02-03 23:36:05 +0100

    mapRenderer + eventHandlers

--- src/projectRaycast/MapRenderer.java
+++ src/projectRaycast/MapRenderer.java
@@ -1,5 +1,84 @@
 package projectRaycast;
 
-public class MapRenderer {
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
 
+public class MapRenderer {
+	private int [][] map = {
+			  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+			  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+			  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+			  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+			};
+	GraphicsContext gc;
+	Canvas canvas;
+
+	public MapRenderer (GraphicsContext gc,Canvas canvas, Scene scene) {
+		this.gc = gc;
+		this.canvas = canvas;
+		gc.setStroke(Color.BLUE);
+        gc.setLineWidth(1);      
+	}
+	
+	public void renderMap (double x, double y) {
+		gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        double squareSize = canvas.getWidth()/map.length;
+		for (int i=0; i<map.length;i++) {
+			for (int j=0; j<map.length;j++) {
+				switch (map[i][j]) {
+				case 0:
+					gc.setFill(Color.LIGHTBLUE);
+					break;
+				case 1:
+					gc.setFill(Color.GRAY);
+					break;
+				case 2:
+					gc.setFill(Color.RED);
+					break;
+				case 3:
+					gc.setFill(Color.GREEN);
+					break;
+				case 4:
+					gc.setFill(Color.BLUE);
+					break;
+				case 5:
+					gc.setFill(Color.YELLOW);
+					break;
+				}
+				gc.fillRect(j*squareSize, i*squareSize, squareSize, squareSize);
+			}
+		}
+		gc.setFill(Color.BLACK);
+		gc.fillOval(x, y, 5, 5);
+		//gc.strokeLine(canvas.getWidth()/2, canvas.getHeight()/2, x, y);
+	}
 }
+	
+	
+	
+	
+
--- src/projectRaycast/TheGame.java
+++ src/projectRaycast/TheGame.java
@@ -2,34 +2,51 @@ package projectRaycast;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 public class TheGame extends Application {
-
+	double playerPositionX = 300;
+    double playerPositionY = 300;
 	public static void main(String[] args) {
 		launch(args);
-
+	}
+	
+	public void setEventHandlers (Scene scene) {
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case UP:    playerPositionY -= 1; break;
+                    case DOWN:  playerPositionY += 1; break;
+                    case LEFT:  playerPositionX -= 1; break;
+                    case RIGHT: playerPositionX += 1; break;
+                }
+            }
+        });
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+        
 		primaryStage.setTitle("raycastEngine");
 		Group root = new Group();
-		Canvas canvas = new Canvas(600, 480);	
-		GraphicsContext gc = canvas.getGraphicsContext2D();		
-		gc.setStroke(Color.BLUE);
-        gc.setLineWidth(5);
+		Canvas canvas = new Canvas(600, 600);	
+		GraphicsContext gc = canvas.getGraphicsContext2D();	
         root.getChildren().add(canvas);
-        primaryStage.setScene(new Scene(root));
-        gc.setStroke(Color.BLUE);
-        gc.setLineWidth(1);
+        Scene scene = new Scene(root);
+        setEventHandlers(scene);
+        primaryStage.setScene(scene);
         final long startNanoTime = System.nanoTime();
+        MapRenderer mapRenderer = new MapRenderer(gc, canvas, scene);
         new AnimationTimer()
 	    {
         	long previousFrameTime = System.nanoTime();
@@ -39,11 +56,8 @@ public class TheGame extends Application {
 				timeCounter += (currentNanoTime-previousFrameTime)/1000000000.0*60;
 				double t = (currentNanoTime - startNanoTime) / 1000000000.0;
 				if (timeCounter>1)  {
-		        	double x = 300 + 100 * Math.cos(t);
-		            double y = 240 + 100 * Math.sin(t);
-		            gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-		            gc.strokeLine(300, 240, x, y);
-					timeCounter-=1;
+					mapRenderer.renderMap(playerPositionX, playerPositionY);
+		            timeCounter-=1;
 				}
 				previousFrameTime = currentNanoTime;
 	        }
