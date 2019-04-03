dd6dc416c8a6f22cc9787f342cba4a276ecd4415
vnskln <37518904+vnskln@users.noreply.github.com>
2019-04-03 23:07:37 +0200

    raycast engine v0.5

--- src/projectRaycast/MapRenderer.java
+++ src/projectRaycast/MapRenderer.java
@@ -6,50 +6,36 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+
+import java.lang.*;
+import java.util.concurrent.TimeUnit;
 
 public class MapRenderer {
-	private int [][] map = {
-			  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-			  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
-			  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-			};
+	
+	Map map;
 	GraphicsContext gc;
 	Canvas canvas;
+	double squareSize;
 
-	public MapRenderer (GraphicsContext gc,Canvas canvas, Scene scene) {
+	public MapRenderer (GraphicsContext gc,Canvas canvas, Scene scene, Map map) {
+		this.map = map;
 		this.gc = gc;
 		this.canvas = canvas;
-		gc.setStroke(Color.BLUE);
+		this.squareSize = canvas.getHeight()/map.getMapLength();
+		gc.setStroke(Color.RED);
         gc.setLineWidth(1);      
 	}
 	
-	public void renderMap (double x, double y) {
+	public void renderMap () {
+		
+		//create blank screen
 		gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        double squareSize = canvas.getWidth()/map.length;
-		for (int i=0; i<map.length;i++) {
-			for (int j=0; j<map.length;j++) {
-				switch (map[i][j]) {
+		
+		//draw map squares on left part of screen
+		for (int i=0; i<map.getMapLength();i++) {
+			for (int j=0; j<map.getMapHeight();j++) {
+				switch (map.getSquareType(i, j)) {
 				case 0:
 					gc.setFill(Color.LIGHTBLUE);
 					break;
@@ -72,9 +58,164 @@ public class MapRenderer {
 				gc.fillRect(j*squareSize, i*squareSize, squareSize, squareSize);
 			}
 		}
+		
+		//draw map lines on left part of screen
+		for (int i=0; i<map.getMapLength(); i++) {
+			gc.setStroke(Color.GRAY);
+			gc.strokeLine(i*squareSize,0,i*squareSize,map.getMapHeight()*squareSize);
+			gc.strokeLine(0,i*squareSize,map.getMapHeight()*squareSize,i*squareSize);
+		}
+	}
+	
+	public void updatePlayerOnMap (double playerPositionX, double playerPositionY, 
+			double playerDirectionX, double playerDirectionY, double planeX, double planeY) {
+
+		//draw player on left side of screen
 		gc.setFill(Color.BLACK);
-		gc.fillOval(x, y, 5, 5);
-		//gc.strokeLine(canvas.getWidth()/2, canvas.getHeight()/2, x, y);
+		gc.fillOval(playerPositionY*squareSize-2, playerPositionX*squareSize-2, 4, 4);
+		
+		//draw field of view on left side of screen
+		gc.strokeLine(playerPositionY*squareSize,
+					  playerPositionX*squareSize, 
+					  playerPositionY*squareSize+500*(playerDirectionY+planeY),
+				      playerPositionX*squareSize+500*(playerDirectionX+planeX));
+		gc.strokeLine(playerPositionY*squareSize, 
+					  playerPositionX*squareSize, 
+					  playerPositionY*squareSize+500*(playerDirectionY-planeY),
+				      playerPositionX*squareSize+500*(playerDirectionX-planeX));
+		gc.setFill(Color.rgb(250,128,114,0.5));
+		gc.fillPolygon(new double[] {playerPositionY*squareSize,
+				  	   playerPositionY*squareSize+800*(playerDirectionY+planeY),
+				       playerPositionY*squareSize+800*(playerDirectionY-planeY)},
+				       new double[] {playerPositionX*squareSize,
+					   playerPositionX*squareSize+800*(playerDirectionX+planeX),
+					   playerPositionX*squareSize+800*(playerDirectionX-planeX)}, 
+					   3);
+	}
+	
+	public void renderView (double playerPositionX, double playerPositionY, 
+				double playerDirectionX, double playerDirectionY, double planeX, double planeY) {
+		//clear right side of screen
+		gc.clearRect(map.getMapLength()*squareSize, 0, canvas.getWidth(), canvas.getHeight());
+		
+		//render right part of screen
+		for (double x = 0; x <= canvas.getWidth()/2;x++) {
+			
+			//calculating ray position
+			double screenX = 2 * x / (canvas.getWidth()/2) - 1;
+			double rayDirectionX = (playerDirectionX + planeX * screenX);
+			double rayDirectionY = (playerDirectionY + planeY * screenX);
+			
+			//calculating current map square
+			int currentSquareX = (int)(playerPositionX);
+		    int currentSquareY = (int)(playerPositionY);
+		    
+		    //distance to next x-wall
+		    double distanceToNextXLine;
+		    
+		    //distance to next y-wall
+		    double distanceToNextYLine;
+		    
+		    //distance between x sides
+		    double distanceBetweenXLines = Math.abs(1 / rayDirectionX);
+		    
+		    //distance between y sides
+		    double distanceBetweenYLines = Math.abs(1 / rayDirectionY);
+		    
+		    //perpendicular distance from player to hit wall
+		    double distanceToHitWall;
+		    
+		    //direction to step in x or y-direction
+		    int stepX;
+		    int stepY;
+		    
+		    //wall hit indicator
+		    int hit = 0;
+		    
+		    //NS or EW hit indicator
+	        int whatSide = 0;
+	        
+	        //prepare calculation
+	        if (rayDirectionX < 0)
+	        {
+	        	stepX = -1;
+	        	distanceToNextXLine = (playerPositionX - currentSquareX) * distanceBetweenXLines;
+	        }
+	        else
+	        {
+	        	stepX = 1;
+	        	distanceToNextXLine = (currentSquareX + 1.0 - playerPositionX) * distanceBetweenXLines;
+	        }
+	        if (rayDirectionY < 0)
+	        {
+	        	stepY = -1;
+	        	distanceToNextYLine = (playerPositionY - currentSquareY) * distanceBetweenYLines;
+	        }
+	        else
+	        {
+	        	stepY = 1;
+	        	distanceToNextYLine = (currentSquareY + 1.0 - playerPositionY) * distanceBetweenYLines;
+	        }
+	        
+	        //digital differential analyzer algorithm used for drawing a line
+	        while (hit == 0)
+		    {
+		    	//jump to next map square, OR in x-direction, OR in y-direction
+		        if (distanceToNextXLine < distanceToNextYLine)
+		        {
+		          distanceToNextXLine += distanceBetweenXLines;
+		          currentSquareX += stepX;
+		          whatSide = 0;
+		        }
+		        else
+		        {
+		          distanceToNextYLine += distanceBetweenYLines;
+		          currentSquareY += stepY;
+		          whatSide = 1;
+		        }
+		        if (map.getSquareType(currentSquareX, currentSquareY) > 0) hit = 1;
+		    }
+	        
+			//Calculate distance projected on camera direction 
+			if (whatSide == 0) 
+				distanceToHitWall = (currentSquareX - playerPositionX + (1 - stepX) / 2) / rayDirectionX;
+			else           
+				distanceToHitWall = (currentSquareY - playerPositionY + (1 - stepY) / 2) / rayDirectionY;
+			
+			//Calculate height of line to draw on screen
+			int h=(int)canvas.getHeight();
+		    int lineHeight = (int)(h / distanceToHitWall);
+		    
+		    //Calculate lowest and highest pixel of line
+			int drawStart = -lineHeight / 2 + h / 2;
+			if(drawStart < 0)drawStart = 0;
+			int drawEnd = lineHeight / 2 + h / 2;
+			if(drawEnd >= h)drawEnd = h - 1;
+			
+			Color chosenColor = Color.WHITE;
+			switch(map.getSquareType(currentSquareX, currentSquareY))
+		      {
+		        case 1:  
+		        	chosenColor=Color.GREY;  
+		        	break;
+		        case 2:  
+		        	chosenColor=Color.RED;  
+		        	break;
+		        case 3:
+		        	chosenColor=Color.GREEN;  
+		        	break;
+		        case 4:
+		        	chosenColor=Color.BLUE;  
+		        	break;
+		        case 5:  
+		        	chosenColor=Color.YELLOW;  
+		        	break;
+		      }
+			if (whatSide == 1)
+				chosenColor = chosenColor.darker();
+			gc.setStroke(chosenColor);
+			gc.strokeLine(x+canvas.getWidth()/2, drawStart, x+canvas.getWidth()/2, drawEnd);
+		}
 	}
 }
 	
--- src/projectRaycast/TheGame.java
+++ src/projectRaycast/TheGame.java
@@ -14,57 +14,100 @@ import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 public class TheGame extends Application {
-	double playerPositionX = 300;
-    double playerPositionY = 300;
-	public static void main(String[] args) {
-		launch(args);
-	}
-	
-	public void setEventHandlers (Scene scene) {
+	//game settings
+	Map map = new Map();
+	double playerPositionX = 4;
+    double playerPositionY = 12;
+    double playerDirectionX = -1;
+    double playerDirectionY = 0;
+    double planeX = 0;
+    double planeY = 0.66;
+    double playerSpeed = 0.1;
+    double playerRotationSpeed = 0.1;
+    //end of game settings
+    
+	//controllers setup
+    public void setEventHandlers (Scene scene) {
 		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
+            	double previousDirectionX;
+            	double oldPlaneX;
                 switch (event.getCode()) {
-                    case UP:    playerPositionY -= 1; break;
-                    case DOWN:  playerPositionY += 1; break;
-                    case LEFT:  playerPositionX -= 1; break;
-                    case RIGHT: playerPositionX += 1; break;
+                    case UP: 
+                    	if (map.getSquareType((int)(playerPositionX+playerDirectionX*playerSpeed), (int)playerPositionY)==0)
+                    		playerPositionX += playerDirectionX*playerSpeed;
+                    	if (map.getSquareType((int)(playerPositionX), (int)(playerPositionY+playerDirectionY*playerSpeed))==0)
+                        	playerPositionY += playerDirectionY*playerSpeed; 
+                    	break;
+                    case DOWN:
+                    	if (map.getSquareType((int)(playerPositionX-playerDirectionX*playerSpeed), (int)playerPositionY)==0)
+                    		playerPositionX -= playerDirectionX*playerSpeed;
+                    	if (map.getSquareType((int)(playerPositionX), (int)(playerPositionY-playerDirectionY*playerSpeed))==0)
+                        	playerPositionY -= playerDirectionY*playerSpeed; 
+                    	break;
+                    case LEFT:
+                    	previousDirectionX = playerDirectionX;
+                    	playerDirectionX = playerDirectionX * Math.cos(playerRotationSpeed) - playerDirectionY * Math.sin(playerRotationSpeed);
+                    	playerDirectionY = previousDirectionX * Math.sin(playerRotationSpeed) + playerDirectionY * Math.cos(playerRotationSpeed);
+                        oldPlaneX = planeX;
+                        planeX = planeX * Math.cos(playerRotationSpeed) - planeY * Math.sin(playerRotationSpeed);
+                        planeY = oldPlaneX * Math.sin(playerRotationSpeed) + planeY * Math.cos(playerRotationSpeed); 
+                    	break;
+                    case RIGHT: 
+                    	previousDirectionX = playerDirectionX;
+                    	playerDirectionX = playerDirectionX * Math.cos(-playerRotationSpeed) - playerDirectionY * Math.sin(-playerRotationSpeed);
+                    	playerDirectionY = previousDirectionX * Math.sin(-playerRotationSpeed) + playerDirectionY * Math.cos(-playerRotationSpeed);
+                        oldPlaneX = planeX;
+                        planeX = planeX * Math.cos(-playerRotationSpeed) - planeY * Math.sin(-playerRotationSpeed);
+                        planeY = oldPlaneX * Math.sin(-playerRotationSpeed) + planeY * Math.cos(-playerRotationSpeed); 
+                    	break; 
                 }
             }
         });
 	}
+    //end of controllers setup
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
         
+		//preparing javafx stage elements
 		primaryStage.setTitle("raycastEngine");
 		Group root = new Group();
-		Canvas canvas = new Canvas(600, 600);	
+		Canvas canvas = new Canvas(1200, 600);	
 		GraphicsContext gc = canvas.getGraphicsContext2D();	
         root.getChildren().add(canvas);
         Scene scene = new Scene(root);
         setEventHandlers(scene);
         primaryStage.setScene(scene);
+        MapRenderer mapRenderer = new MapRenderer(gc, canvas, scene, map);
+        
+        //preparing game loop
         final long startNanoTime = System.nanoTime();
-        MapRenderer mapRenderer = new MapRenderer(gc, canvas, scene);
         new AnimationTimer()
 	    {
         	long previousFrameTime = System.nanoTime();
         	double timeCounter = 0;
         	public void handle(long currentNanoTime)
 	        {
-				timeCounter += (currentNanoTime-previousFrameTime)/1000000000.0*60;
-				double t = (currentNanoTime - startNanoTime) / 1000000000.0;
-				if (timeCounter>1)  {
-					mapRenderer.renderMap(playerPositionX, playerPositionY);
-		            timeCounter-=1;
+				timeCounter += (currentNanoTime-previousFrameTime)/1000000000.0*120;
+				if (timeCounter>1) {
+					
+					//main game routine
+					mapRenderer.renderMap();
+					mapRenderer.updatePlayerOnMap(playerPositionX, playerPositionY,playerDirectionX, playerDirectionY,planeX, planeY);
+					mapRenderer.renderView(playerPositionX, playerPositionY,playerDirectionX, playerDirectionY,planeX, planeY);
+					//end of main game routine
+		            timeCounter=0;
 				}
 				previousFrameTime = currentNanoTime;
 	        }
 	    }.start();
-		
 		primaryStage.show();
-		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
 
 }
